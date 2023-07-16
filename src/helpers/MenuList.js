8d8965576f7381e9f1404151b196c64b9fa2d0f0
Daniela Flores <danielafloresavila.02@gmail.com>
2023-07-16 15:31:29 +0100

    Add the project

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,40 @@
+import Pepperoni from '../assets/pepperoni.jpg';
+import Margherita from '../assets/margherita.jpg';
+import PedroTechSpecial from '../assets/pedrotechspecial.jpg';
+import Vegan from '../assets/vegan.jpg';
+import Pineapple from '../assets/pineapple.jpg';
+import VeryExpensive from '../assets/expensive.jpg';
+
+export const MenuList = [
+    {
+        name:"Pepperoni Pizza",
+        image: Pepperoni,
+        price: 12.99,
+    },
+    {
+        name:"Margherita Pizza",
+        image: Margherita,
+        price: 11.99,
+    },
+    {
+        name:"PedroTech Special Pizza",
+        image: PedroTechSpecial,
+        price: 99.99,
+    }, 
+    {
+        name:"Vegan Pizza",
+        image: Vegan,
+        price: 15.99,
+    },
+    {
+        name:"Pineapple Pizza",
+        image: Pineapple,
+        price: 10.99,
+    },
+    {
+        name:"Very Expensive Pizza",
+        image: VeryExpensive,
+        price: 150.45
+    },
+     
+]
